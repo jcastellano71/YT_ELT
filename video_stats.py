1aaf63de23782be52df48e3b223c79c2f2f21eb7
jcastellano71 <jorge.castellano@hotmail.com>
2026-05-12 12:41:08 -0400

    Added .env to .gitignore and saved API_KEY inside the .env file

---
+++ video_stats.py
@@ -0,0 +1,41 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+#API_KEY = "AIzaSyD4ZP6t_L95XFh-R6zTWt0CJkyo68nVwYY"
+load_dotenv(dotenv_path="./.env")
+API_KEY = os.getenv("API_KEY")
+
+CHANNEL_HANDLE ="MrBeast"
+
+def get_playlist_id():
+
+    try:
+        url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
+
+        response = requests.get(url)
+        response.raise_for_status()
+        #print(response)
+
+        data = response.json()
+
+        #print(json.dumps(data, indent=4))
+
+        # uploads variable is the ID of the playlist
+        channel_itmes = data["items"][0] # Gotten using Json Crack Extension
+
+        channel_playlistID = channel_itmes["contentDetails"]["relatedPlaylists"]["uploads"] 
+
+        print(channel_playlistID)
+
+        return channel_playlistID
+
+    except requests.exceptions.RequestException as e:
+        raise e
+    
+if __name__ == "__main__": #__name__ inbuilt python var that is set depending on how the script is run. __main__ is when the script is run directly and no importes as module. If the script is run from another script __name__ is not equal to __main_. __name__ will be set to the script name
+    print("get_playlist_id will be executed")
+    get_playlist_id()
+else:
+    print("get_playlist_id won't be executed")
